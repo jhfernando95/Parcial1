e333e037b865e5ce1dbb34cab07de8f045c38322
aula7 <pc714@gmail.com>
2017-03-15 13:55:34 -0500

    joh

--- app/src/main/java/com/example/aula7/parcial1/MainActivity.java
+++ app/src/main/java/com/example/aula7/parcial1/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.aula7.parcial1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class LoginActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+        public void goCreateAccount(View view){
+            Intent intent = new Intent(this, class);
+            startActivity(intent);
+        }
     }
-}
+
